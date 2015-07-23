7951c95dac6452d5990b1df273c829680448c96d
Andrew Nichols <aln2129@gmail.com>
2015-07-23 11:36:52 -0400

    sys.call

--- Shiny-Project/Xavier/helpers.R
+++ Shiny-Project/Xavier/helpers.R
@@ -110,7 +110,7 @@ graph_real_time<-function(file,subwayline,realTimeDirection){
   ##############################
   
   ## modified script for nyc data science server
-  fileData <- system("/usr/bin/python mtaRealTime.py", intern=TRUE)
+  fileData <- sys.call("/usr/bin/python mtaRealTime.py", intern=TRUE)
   tester <- sapply(fileData,function (x) {strsplit(x,split = ",")} )
   filetxt <- as.data.frame(matrix(ncol = 10))
   for(i in 1:length(tester)) {
@@ -219,3 +219,4 @@ graph002 = ggplot() +
   #coord_fixed()
 graph002
 }
+
