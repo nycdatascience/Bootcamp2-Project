6f344cb1420ac09004362d94486f53c2a550c2f8
Andrew Nichols <aln2129@gmail.com>
2015-09-04 14:04:28 -0400

    Change the path for python call in console

--- Shiny-Project/Xavier/helpers.R
+++ Shiny-Project/Xavier/helpers.R
@@ -110,7 +110,7 @@ graph_real_time<-function(file,subwayline,realTimeDirection){
   ##############################
   
   ## modified script for nyc data science server
-  fileData <- system("/Users/andrew/anaconda/bin/python mtaRealTime.py", intern=TRUE)
+  fileData <- system("/usr/bin/python mtaRealTime.py", intern=TRUE)
   tester <- sapply(fileData,function (x) {strsplit(x,split = ",")} )
   filetxt <- as.data.frame(matrix(ncol = 10))
   for(i in 1:length(tester)) {
